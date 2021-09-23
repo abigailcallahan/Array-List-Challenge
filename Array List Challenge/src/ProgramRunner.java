7914232d91f68ec80ea5f6dbd501b98515e9ef3f
abigail.callahan <abigail.callahan@MU-C1-02.mullen.pvt.k12.co.us>
2021-09-23 11:38:15 -0600

    added changes to the printList method

--- Array List Challenge/src/ProgramRunner.java
+++ Array List Challenge/src/ProgramRunner.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 public class ProgramRunner
 	{
+		static int longestWord = 0;
 		public static void main(String[] args)
 		{
 		//Program is the name of the class I am pulling from
@@ -14,13 +15,17 @@ public class ProgramRunner
 		TVShows.add(new Program("Outer Banks", "Mystery", 2));
 		
 		printList();
+		System.out.println("This is a test to see if I can put it back into GitHub");
 		}
 		
 		public static void printList()
 		{
 			for(int i = 0; i < TVShows.size(); i ++)
 				{
-					
+					if(TVShows.get(i) > longestWord)
+						{
+							
+						}
 				}
 		}
 		
